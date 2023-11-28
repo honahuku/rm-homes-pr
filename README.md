a0f7eee1177833a111ecf59eb0e63526e89ab943
Honahuku <tech@honahuku.com>
2023-11-28 10:40:46 +0900

    feat: スクリプトを追加

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rm-homes-pr
 homesのPRを非表示にするやつ
+
+## greasyfork
+[homesのPRを非表示にするやつ](https://greasyfork.org/ja/scripts/480915-homes%E3%81%AEpr%E3%82%92%E9%9D%9E%E8%A1%A8%E7%A4%BA%E3%81%AB%E3%81%99%E3%82%8B%E3%82%84%E3%81%A4)
---
+++ main.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         homesのPRを非表示にするやつ
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  homes.co.jpの特定のページで特定のdivを非表示にする
+// @author       Honahuku
+// @match        https://www.homes.co.jp/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function hidePRDivs() {
+        var icons = document.querySelectorAll('span.icon');
+        icons.forEach(function(icon) {
+            if (icon.textContent.trim() === 'PR') {
+                var parentDiv = icon.closest('div.ui-frame');
+                if (parentDiv) {
+                    parentDiv.style.display = 'none';
+                }
+            }
+        });
+    }
+
+    // ページが読み込まれたら実行する
+    window.addEventListener('load', function() {
+        hidePRDivs();
+
+        // searchResult要素の変更を監視する
+        var observer = new MutationObserver(hidePRDivs);
+        var target = document.getElementById('searchResult');
+        if (target) {
+            observer.observe(target, { childList: true, subtree: true });
+        }
+    }, false);
+})();
